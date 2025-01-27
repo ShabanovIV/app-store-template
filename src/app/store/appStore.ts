251c14b724ed323f02923122c4c508b1fc657595
ShabanovIV <react.dev.shabanov@mail.ru>
2025-01-27 09:56:44 +0500

    refactor: move store and rename

--- src/app/store/appStore.ts
+++ src/app/store/appStore.ts
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userApi } from 'src/entities/User/model/userApi';
 
-export const store = configureStore({
+export const appStore = configureStore({
   reducer: {
     [userApi.reducerPath]: userApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userApi.middleware),
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof appStore.getState>;
+export type AppDispatch = typeof appStore.dispatch;
