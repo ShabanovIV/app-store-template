e5ed2f9965a125a810ba9d46de78ac21301d1cfb
ShabanovIV <react.dev.shabanov@mail.ru>
2025-01-29 20:12:02 +0500

    refactor: moved AppMenu to features
    - Moved from widgets to features/AppMenu as it was violating FSD imports;
    - Improved code structure and modularity.

--- src/widgets/AppHeader/ui/AppHeader.tsx
+++ src/widgets/AppHeader/ui/AppHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Header } from 'antd/es/layout/layout';
+import { AppMenu } from 'src/features/AppMenu';
 import { Logo } from 'src/shared/ui/Logo/Logo';
-import { AppMenu } from 'src/widgets/AppMenu/ui/AppMenu';
 import styles from './AppHeader.module.scss';
 
 export const AppHeader: React.FC = () => {
