c07e2bb9a778713b541aa1a3206a0bfc90f933a9
ShabanovIV <react.dev.shabanov@mail.ru>
2025-02-03 20:56:17 +0500

    feature: add RenderList and RenderListObserver
    - Implemented `RenderList` component for list rendering;
    - Implemented `RenderListObserver` to track the last item in the list.

---
+++ src/shared/ui/RenderList/RenderList.tsx
@@ -0,0 +1,69 @@
+import React, { forwardRef, memo, ReactNode } from 'react';
+import styles from './RenderList.module.scss';
+
+export interface IRenderItem {
+  key: string;
+  render: () => ReactNode;
+  onButtonEditClick?: () => void;
+}
+
+export interface IRenderListProps {
+  isGrid?: boolean;
+  items: IRenderItem[];
+  lastItemRef?: React.RefObject<HTMLLIElement | null>;
+  onLastItemChanged: (key: string) => void;
+}
+
+export const RenderList: React.FC<IRenderListProps> = ({
+  isGrid,
+  items,
+  lastItemRef,
+  onLastItemChanged,
+}) => {
+  if (items.length === 0) {
+    return <div className={styles.empty}>No items to display</div>;
+  }
+
+  const memoItems = items.map((item, index) => (
+    <MemoizedListItem
+      key={item.key}
+      item={item}
+      ref={setRefForLast({ items, index, onLastItemChanged, lastItemRef })}
+    />
+  ));
+
+  return <ul className={`${styles.list} ${isGrid ? styles.listGrid : ''}`}>{memoItems}</ul>;
+};
+
+type ListItemProps<TItem> = {
+  item: TItem;
+};
+
+const ListItem = forwardRef<HTMLLIElement, ListItemProps<IRenderItem>>(({ item }, ref) => (
+  <li id={item.key} ref={ref} className={styles.listItem}>
+    {item.render()}
+  </li>
+));
+
+ListItem.displayName = 'ListItem';
+
+const MemoizedListItem = memo(ListItem);
+
+const setRefForLast = ({
+  items,
+  index,
+  onLastItemChanged,
+  lastItemRef,
+}: {
+  items: IRenderItem[];
+  index: number;
+  onLastItemChanged: (key: string) => void;
+  lastItemRef?: React.RefObject<HTMLLIElement | null>;
+}) => {
+  return (element: HTMLLIElement | null) => {
+    if (index === items.length - 1 && lastItemRef) {
+      lastItemRef.current = element;
+      onLastItemChanged(items[items.length - 1].key);
+    }
+  };
+};
---
+++ src/shared/ui/RenderList/RenderListObserver.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useRef, useCallback, useState } from 'react';
+import { RenderList, IRenderItem } from './RenderList';
+
+export interface IRenderListObserverProps {
+  isGrid?: boolean;
+  items: IRenderItem[];
+  onLastItem: () => void;
+}
+
+const RenderListObserver: React.FC<IRenderListObserverProps> = ({ isGrid, items, onLastItem }) => {
+  const [lastItemKey, setLastItemKey] = useState('');
+  const prevLastItemKey = useRef('');
+  const lastItemRef = useRef<HTMLLIElement | null>(null);
+  const hasCalledOnLastItem = useRef(false);
+
+  const handleIntersection = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const entry = entries[0];
+      if (entry?.isIntersecting && !hasCalledOnLastItem.current) {
+        hasCalledOnLastItem.current = true;
+        onLastItem();
+      }
+    },
+    [onLastItem],
+  );
+
+  useEffect(() => {
+    if (lastItemKey === prevLastItemKey.current) {
+      return;
+    } else {
+      prevLastItemKey.current = lastItemKey;
+    }
+
+    const observer = new IntersectionObserver(handleIntersection, { threshold: 1 });
+
+    const currentLastItem = lastItemRef.current; // Сохраняем текущее значение рефа
+    if (currentLastItem) {
+      observer.observe(currentLastItem);
+    }
+
+    return () => {
+      if (currentLastItem) {
+        observer.unobserve(currentLastItem); // Используем сохранённое значение
+      }
+    };
+  }, [handleIntersection, lastItemKey]);
+
+  useEffect(() => {
+    hasCalledOnLastItem.current = false; // Сбрасываем флаг при изменении списка
+  }, [lastItemKey]);
+
+  return (
+    <RenderList
+      isGrid={isGrid}
+      items={items}
+      lastItemRef={lastItemRef}
+      onLastItemChanged={setLastItemKey}
+    />
+  );
+};
+
+export default RenderListObserver;
