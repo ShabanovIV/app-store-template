5e8d0b2415e25dc6808f0bba4270f11bf5254b69
ShabanovIV <react.dev.shabanov@mail.ru>
2025-02-08 00:20:32 +0500

    feature: add AddToCart for ProductList items

--- src/widgets/lists/ProductList/ui/convertProductToItem.tsx
+++ src/widgets/lists/ProductList/ui/convertProductToItem.tsx
@@ -1,20 +1,20 @@
 import { Product } from 'src/entities/Product/types/product';
 import { ProductCard } from 'src/entities/Product/ui/ProductCard';
+import AddToCart from 'src/features/Cart/ui/AddToCart';
 import { IRenderItem } from 'src/shared/ui/RenderList/IRenderItem';
 
 interface ConvertProps {
   product: Product;
   onClick?: (id: string) => void;
-  footer?: () => React.ReactNode;
 }
 
-export const convertProductToItem = ({ product, onClick, footer }: ConvertProps): IRenderItem => ({
+export const convertProductToItem = ({ product, onClick }: ConvertProps): IRenderItem => ({
   key: product.id,
   render: () => (
     <ProductCard
       product={product}
       onClick={onClick ? () => onClick(product.id) : undefined}
-      footer={footer}
+      footer={() => <AddToCart productId={product.id} />}
     />
   ),
 });
